c249d2a7f06f71cd75d020c120cfb9f0c0e0e61f
fakulp <agus.lp.md@gmail.com>
2025-02-07 18:16:00 -0300

    preentrega1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { NavBar } from './components/NavBar'
+import { ListContainer } from './components/ItemListContainer'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <NavBar/>
+    <ListContainer titulo ="Lion" texto="Miniatura" image="imagen" />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,1 @@
+export const CartWidget = () => <span>carrito</span>
